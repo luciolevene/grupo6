383334246ffe8ad8d4708b5172baf2c952f419a4
JoaquinCelentano <jcelentanoalonso@udesa.edu.ar>
2025-09-17 15:42:57 -0300

    se completaron las rutas en main controller con procuctos de prueba

--- Grupo6/controllers/mainController.js
+++ Grupo6/controllers/mainController.js
@@ -1,6 +1,29 @@
+// controllers/mainController.js
 const mainController = {
-    index: function(req, res) {
-        res.render('index'); 
+    index: (req, res) => {
+        // Datos de prueba hasta tener base de datos
+        const productos = [
+            { id: 1, nombre: "Cafetera Moulinex", descripcion: "Cafetera eléctrica automática", comentarios: 12 },
+            { id: 2, nombre: "Televisor Samsung", descripcion: "Smart TV 50 pulgadas", comentarios: 8 },
+            { id: 3, nombre: "Notebook HP", descripcion: "Core i5, 8GB RAM", comentarios: 20 }
+        ];
+        res.render("index", { productos }); 
+    },
+
+    login: (req, res) => {
+        res.render("login");
+    },
+
+    register: (req, res) => {
+        res.render("register");
+    },
+
+    profile: (req, res) => {
+        res.render("profile");
+    },
+
+    product: (req, res) => {
+        res.render("product");
     }
 };
 
