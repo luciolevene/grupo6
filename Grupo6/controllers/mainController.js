352a87796be96d11ae5336dc31e57b9493645a25
JoaquinCelentano <jcelentanoalonso@udesa.edu.ar>
2025-09-17 15:53:33 -0300

    se agregaro imagenes de los productos en maincontroller y se colocaron en public para que se muestren en el ejs

--- Grupo6/controllers/mainController.js
+++ Grupo6/controllers/mainController.js
@@ -1,12 +1,37 @@
-// controllers/mainController.js
 const mainController = {
     index: (req, res) => {
         // Datos de prueba hasta tener base de datos
         const productos = [
-            { id: 1, nombre: "Cafetera Moulinex", descripcion: "Cafetera eléctrica automática", comentarios: 12 },
-            { id: 2, nombre: "Televisor Samsung", descripcion: "Smart TV 50 pulgadas", comentarios: 8 },
-            { id: 3, nombre: "Notebook HP", descripcion: "Core i5, 8GB RAM", comentarios: 20 }
+            { 
+                id: 1, 
+                nombre: "Cafetera Moulinex", 
+                descripcion: "Cafetera eléctrica automática", 
+                comentarios: 12,
+                imagen: "/images/products/img-cafetera-moulinex.jpg"
+            },
+            { 
+                id: 2, 
+                nombre: "Televisor Samsung", 
+                descripcion: "Smart TV 50 pulgadas", 
+                comentarios: 8,
+                imagen: "/images/products/img-tv-samsung-smart.jpg"
+            },
+            { 
+                id: 3, 
+                nombre: "Notebook HP", 
+                descripcion: "Core i5, 8GB RAM", 
+                comentarios: 20,
+                imagen: "/images/products/img-macbook-pro-2019.jpg"
+            },
+            { 
+                id: 4, 
+                nombre: "Celular Samsung Galaxy S10", 
+                descripcion: "Celular alta gama", 
+                comentarios: 15,
+                imagen: "/images/products/img-samsung-galaxy-s10.jpg"
+            }
         ];
+
         res.render("index", { productos }); 
     },
 
