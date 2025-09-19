82a5ecdb3d07ddc51449a44204cf7d18a4e5744a
luciolevene <llevene@udesa.edu.ar>
2025-09-19 16:10:52 -0300

    arreglando controlador y db

--- Grupo6/controllers/mainController.js
+++ Grupo6/controllers/mainController.js
@@ -1,53 +1,25 @@
-const mainController = {
-    index: (req, res) => {
-        // Datos de prueba hasta tener base de datos
-        const productos = [
-            { 
-                id: 1, 
-                nombre: "Cafetera Moulinex", 
-                descripcion: "Cafetera eléctrica automática", 
-                comentarios: 12,
-                imagen: "/images/products/img-cafetera-moulinex.jpg"
-            },
-            { 
-                id: 2, 
-                nombre: "Televisor Samsung", 
-                descripcion: "Smart TV 50 pulgadas", 
-                comentarios: 8,
-                imagen: "/images/products/img-tv-samsung-smart.jpg"
-            },
-            { 
-                id: 3, 
-                nombre: "Notebook HP", 
-                descripcion: "Core i5, 8GB RAM", 
-                comentarios: 20,
-                imagen: "/images/products/img-macbook-pro-2019.jpg"
-            },
-            { 
-                id: 4, 
-                nombre: "Celular Samsung Galaxy S10", 
-                descripcion: "Celular alta gama", 
-                comentarios: 15,
-                imagen: "/images/products/img-samsung-galaxy-s10.jpg"
-            }
-        ];
+const productos = require("../db");
 
+
+const mainController = {
+    index: function(req, res) {
+        
         res.render("index", { productos }); 
     },
 
-    login: (req, res) => {
+    login: function(req, res) {
         res.render("login");
     },
 
-    register: (req, res) => {
+    register: function(req, res) {
         res.render("register");
     },
 
-    profile: (req, res) => {
+    profile: function(req, res) {
         res.render("profile");
     },
 
-    product: (req, res) => {
+    product: function(req, res)  {
         res.render("product");
     }
 };
--- Grupo6/db/index.js
+++ Grupo6/db/index.js
@@ -1,3 +1,42 @@
+const productos = [
+            { 
+                id: 1, 
+                nombre: "Cafetera Moulinex", 
+                descripcion: "Cafetera eléctrica automática", 
+                comentarios: 12,
+                imagen: "/images/products/img-cafetera-moulinex.jpg"
+            },
+            { 
+                id: 2, 
+                nombre: "Televisor Samsung", 
+                descripcion: "Smart TV 50 pulgadas", 
+                comentarios: 8,
+                imagen: "/images/products/img-tv-samsung-smart.jpg"
+            },
+            { 
+                id: 3, 
+                nombre: "Notebook HP", 
+                descripcion: "Core i5, 8GB RAM", 
+                comentarios: 20,
+                imagen: "/images/products/img-macbook-pro-2019.jpg"
+            },
+            { 
+                id: 4, 
+                nombre: "Celular Samsung Galaxy S10", 
+                descripcion: "Celular alta gama", 
+                comentarios: 15,
+                imagen: "/images/products/img-samsung-galaxy-s10.jpg"
+            }
+        ];
+
+
+
+
+
+
+
+
+
 const data = {
     usuario: {
       id: 1,
@@ -110,5 +149,5 @@ const data = {
     ]
   };
   
-  module.exports = data;
+  module.exports = productos;
   
