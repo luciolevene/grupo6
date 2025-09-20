b5e66cac22415174c51b2822815922cc34e61756
JoaquinCelentano <jcelentanoalonso@udesa.edu.ar>
2025-09-19 22:03:30 -0300

    se agrego el detalle estatico a los productos

--- Grupo6/controllers/productController.js
+++ Grupo6/controllers/productController.js
@@ -2,12 +2,25 @@ const productController = {
     lista: function (req, res) {
         res.render('products/lista');
     },
-    detalle : function (req, res) {
-        res.render('products/detalle');
+
+    detalle: function (req, res) {
+        const producto = {
+            id: 1,
+            nombre: "Cafetera Moulinex",
+            descripcion: "Cafetera eléctrica automática",
+            comentarios: [
+                { usuario: "Juan", texto: "Muy buena cafetera", imagen: "/images/users/profile-default.png" },
+                { usuario: "Ana", texto: "Me encanto muy buena la uso todos los días", imagen: "/images/users/profile-default.png" }
+            ],
+            imagen: "/images/products/img-cafetera-moulinex.jpg"
+        };
+
+        res.render('product', { producto }); 
     },
+
     add: function (req, res) {
-        res.render('products/add');
+        res.render('product-add');
     }
-}
+};
 
 module.exports = productController;
