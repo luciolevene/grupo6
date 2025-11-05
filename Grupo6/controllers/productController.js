c443902e9840025f9016e87d2119a50be6ee4cd9
JoaquinCelentano <jcelentanoalonso@udesa.edu.ar>
2025-11-05 20:54:16 -0300

    se modifico la funcion search para poder buscar prod. en el buscador (punto 5)

--- Grupo6/controllers/productController.js
+++ Grupo6/controllers/productController.js
@@ -1,4 +1,7 @@
 const data = require("../db");
+const db = require('../database/models');
+const { Op } = require('sequelize');
+
 
 const productController = {
     lista: function (req, res) {
@@ -17,10 +20,32 @@ processAdd: function (req, res) {
     res.render('index', { productos: data.productos, logeado: true });
 },
 search: function (req, res) {
-    const products = data.productos.slice(0,3);
-    const search = "celular"; 
-    res.render('search-results', { products, search, logeado: true  });
+  const term = req.query.search;
+
+  db.Product.findAll({
+    where: {
+      nombre: { [db.Sequelize.Op.like]: "%" + term + "%" }
+    },
+    include: [
+      { association: "user" }
+    ],
+    order: [["createdAt", "DESC"]]
+  })
+  .then(function (products) {
+    res.render("search-results", {
+      products: products,
+      search: term
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+    res.render("search-results", {
+      products: [],
+      search: term
+    });
+  });
 },
+
 
     
 
